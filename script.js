92d120e7125f082b1d7d7153ecc235694a2b5074
hasharanethmi2020@gmail.com <hasharanethmi2020@gmail.com>
2023-07-19 12:33:39 +0530

    added search form

--- script.js
+++ script.js
@@ -5,3 +5,16 @@ menue.onclick = () => {
     menue.classList.toggle('fa-times');
     navbar.classList.toggle('active')
 }
+
+window.onscroll = () =>{
+    menu.classList.remove('fa-times');
+    navbar.classList.remove('active');
+}
+
+document.querySelector('#search-icon').onclick = () =>{
+    document.querySelector('#search-form').classList.toggle('active');
+}
+
+document.querySelector('#close').onclick = () =>{
+    document.querySelector('#search-form').classList.remove('active');
+}
