6e47e3f67cb16f3c11b3e881a48b43a10c14928e
hasharanethmi2020@gmail.com <hasharanethmi2020@gmail.com>
2023-07-20 13:31:43 +0530

    added home section

--- script.js
+++ script.js
@@ -18,3 +18,17 @@ document.querySelector('#search-icon').onclick = () =>{
 document.querySelector('#close').onclick = () =>{
     document.querySelector('#search-form').classList.remove('active');
 }
+
+var swiper = new Swiper(".home-slider", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+      delay: 7500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    loop:true,
+  });
